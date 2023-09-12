42b660f70d75d190af3c5e6ccf7c0e37857caab6
Niko-ich <84096556+Niko-ich@users.noreply.github.com>
2023-09-12 15:51:41 +0200

    Added examples.
    - For sliding
    - To reproduce simulations easier

---
+++ run_fullStokes_friction_other_start_value.py
@@ -0,0 +1,122 @@
+import sys
+sys.path.append("../")
+
+from elements.taylor_hood import *
+from equations.full_stokes import *
+from geometries.rectangle_sliding import *
+from solver.picard import Picard
+from solver.newton import Newton
+from step_size_control.exact import *
+from step_size_control.armijo import *
+from step_size_control.constant_step import *
+
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+
+# Creating channel
+#domain = ChannelCylinder()
+dim=2
+
+Domain = RectangleSliding(resolution_x_direction=150,resolution_z_direction=30)
+
+
+print('num cells',Domain.mesh.num_cells())
+
+
+# We visualize our domain
+#RectangleSliding.visualize(Domain)
+
+start = time.time()
+
+Elements = TaylorHood(Domain)
+
+
+alpha=0.5*np.pi/180.0
+g=9.81
+rho=910
+if(dim==2):
+	f = g*rho*Constant((np.sin(alpha),-np.cos(alpha)))
+
+
+#File mesh_file("u.pvd");
+#u_field >> u;
+n= 3 # Exponent for nonlinear term with p=1+1/n
+delta = 10**(-12) # term for differentiation.
+mu0= 10**(-17)	# term for getting solution in H^1.
+B = 0.5*(1.0e-16)**(-1.0/3.0) # B term. Factor infront of nonlinear term.
+realistic_factor = 10**6 # factor that together with B has a typical size of Dv
+
+friction_coefficients = [1e3,1e7]
+
+
+for friction_coefficient in friction_coefficients:
+
+	print('reference calculation')
+	# reference solution
+	equation = FullStokes(Domain,Elements,B,n,delta,mu0,f,friction_domain=Domain.ds(0),friction_coefficient=friction_coefficient)
+	# We solve the problem with a velocity field that we introduce in nue
+	initial_stokes = Stokes(Domain,Elements,B*realistic_factor,f)
+	initial_solve = LinearDirect(initial_stokes)
+	initial_solve.solve()
+	equation.U = initial_stokes.U
+	line_search = ConstantStep()
+	solver = Picard(equation,line_search,max_iter=40,calc_norm=True,output_file='friction_other_start_value/150x30Picard_Reference_friction_coeff_high_res'+str(friction_coefficient))
+	solution_picard_ref = solver.solve()
+
+	print('Newton with armijo')
+	# Newton with armijo
+	equation = FullStokes(Domain,Elements,B,n,delta,mu0,f,friction_domain=Domain.ds(0),friction_coefficient=friction_coefficient)
+	# We solve the problem with a velocity field that we introduce in nue
+	initial_stokes = Stokes(Domain,Elements,B*realistic_factor,f)
+	initial_solve = LinearDirect(initial_stokes)
+	initial_solve.solve()
+	equation.U = initial_stokes.U
+	line_search = Armijo(min_term='functional',gamma=10**(-15))
+	solver = Newton(equation,line_search,max_iter=40,calc_norm=True,output_file='friction_other_start_value/150x30Newton_with_Armijo_friction_coeff_high_res'+str(friction_coefficient))
+	solution_Newton_armijo = solver.solve()
+
+	print('Newton with armijo min_step')
+	# Newton with armijo min_step
+	equation = FullStokes(Domain,Elements,B,n,delta,mu0,f,friction_domain=Domain.ds(0),friction_coefficient=friction_coefficient)
+	# We solve the problem with a velocity field that we introduce in nue
+	initial_stokes = Stokes(Domain,Elements,B*realistic_factor,f)
+	initial_solve = LinearDirect(initial_stokes)
+	initial_solve.solve()
+	equation.U = initial_stokes.U
+	line_search = Armijo(min_term='functional',gamma=10**(-10),min_step=0.5)
+	solver = Newton(equation,line_search,max_iter=40,calc_norm=True,
+				output_file='friction_other_start_value/150x30Newton_with_Armijo_min_step_friction_coeff_high_res'+str(friction_coefficient))
+	solution_Newton_armijo_min_step = solver.solve()
+
+	print('Newton with exact')
+	# Newton with exact
+	equation = FullStokes(Domain,Elements,B,n,delta,mu0,f,friction_domain=Domain.ds(0),friction_coefficient=friction_coefficient)
+	# We solve the problem with a velocity field that we introduce in nue
+	initial_stokes = Stokes(Domain,Elements,B*realistic_factor,f)
+	initial_solve = LinearDirect(initial_stokes)
+	initial_solve.solve()
+	equation.U = initial_stokes.U
+	line_search = Exact(min_term='functional',right=4.0,left=0.0)
+	solver = Newton(equation,line_search,max_iter=40,calc_norm=True,
+				output_file='friction_other_start_value/150x30Newton_with_exact_friction_coeff_high_res'+str(friction_coefficient))
+
+	solution_Newton_exact = solver.solve()
+
+
+	print('Picard exact step size')
+	# Picard exact step size
+	equation = FullStokes(Domain,Elements,B,n,delta,mu0,f,friction_domain=Domain.ds(0),friction_coefficient=friction_coefficient)
+	# We solve the problem with a velocity field that we introduce in nue
+	initial_stokes = Stokes(Domain,Elements,B*realistic_factor,f)
+	initial_solve = LinearDirect(initial_stokes)
+	initial_solve.solve()
+	equation.U = initial_stokes.U
+	line_search =Exact(min_term='functional',right=4.0,left=0.0)
+	solver = Picard(equation,line_search,max_iter=40,calc_norm=True,
+	output_file='friction_other_start_value/150x30Picard_exact_friction_coeff_high_res'+str(friction_coefficient))
+	solution_picard_exact = solver.solve()
+
+end = time.time()
+print('elapsed time',end-start)
---
+++ run_fullStokes_high_resolution.py
@@ -0,0 +1,150 @@
+import sys
+sys.path.append("../")
+
+from elements.taylor_hood import *
+from equations.full_stokes import *
+from geometries.bumpy_bed_3d import *
+from solver.picard import Picard
+from solver.newton import Newton
+from step_size_control.exact import *
+from step_size_control.armijo import *
+from step_size_control.constant_step import *
+
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+
+# Creating channel
+dim=2
+
+Domain = BumpyBed(k=3,resolution_x_direction=700,resolution_z_direction=20,local_refine=False)
+
+
+
+print('num cells',Domain.mesh.num_cells())
+
+# We visualize our domain
+BumpyBed3D.visualize(Domain)
+
+start = time.time()
+
+Elements = TaylorHood(Domain)
+
+
+alpha=0.5*np.pi/180.0
+g=9.81
+rho=910
+if(dim==2):
+	f = g*rho*Constant((np.sin(alpha),-np.cos(alpha)))
+if(dim==3):
+	f = g*rho*Constant((np.sin(alpha),0,-np.cos(alpha)))
+
+
+#File mesh_file("u.pvd");
+#u_field >> u;
+n= 3 # Exponent for nonlinear term with p=1+1/n
+delta = 10**(-12) # term for differentiation.
+mu0= 10**(-17)	# term for getting solution in H^1.
+B = 0.5*(1.0e-16)**(-1.0/3.0) # B term. Factor infront of nonlinear term.
+realistic_factor = 10**6 # factor that together with B has a typical size of Dv
+
+
+print('reference calculation')
+# reference solution
+equation = FullStokes(Domain,Elements,B,n,delta,mu0,f)
+# We solve the problem with a velocity field that we introduce in nue
+initial_stokes = Stokes(Domain,Elements,B*realistic_factor,f)
+initial_solve = LinearDirect(initial_stokes)
+initial_solve.solve()
+equation.U = initial_stokes.U
+line_search = ConstantStep()
+solver = Picard(equation,line_search,max_iter=100,calc_norm=False,output_file='2d_high_resolution/reference_calculation')
+solution_picard_ref = solver.solve()
+
+
+
+print('Newton with armijo')
+# Newton with armijo
+equation = FullStokes(Domain,Elements,B,n,delta,mu0,f)
+# We solve the problem with a velocity field that we introduce in nue
+initial_stokes = Stokes(Domain,Elements,B*realistic_factor,f)
+initial_solve = LinearDirect(initial_stokes)
+initial_solve.solve()
+equation.U = initial_stokes.U
+line_search = Armijo(min_term='functional',gamma=10**(-10))
+solver = Newton(equation,line_search,max_iter=100,calc_norm=True,output_file='2d_high_resolution/Newton_with_Armijo',ref_solution=solution_picard_ref)
+solution_Newton_armijo = solver.solve()
+
+
+print('Newton with armijo min_step')
+# Newton with armijo min_step
+equation = FullStokes(Domain,Elements,B,n,delta,mu0,f)
+# We solve the problem with a velocity field that we introduce in nue
+initial_stokes = Stokes(Domain,Elements,B*realistic_factor,f)
+initial_solve = LinearDirect(initial_stokes)
+initial_solve.solve()
+equation.U = initial_stokes.U
+line_search = Armijo(min_term='functional',gamma=10**(-10),min_step=0.5)
+solver = Newton(equation,line_search,max_iter=100,calc_norm=True,
+				output_file='2d_high_resolution/Newton_with_Armijo_min_step',ref_solution=solution_picard_ref)
+solution_Newton_armijo_min_step = solver.solve()
+
+print('Newton with exact')
+# Newton with exact
+equation = FullStokes(Domain,Elements,B,n,delta,mu0,f)
+# We solve the problem with a velocity field that we introduce in nue
+initial_stokes = Stokes(Domain,Elements,B*realistic_factor,f)
+initial_solve = LinearDirect(initial_stokes)
+initial_solve.solve()
+equation.U = initial_stokes.U
+line_search = Exact(min_term='functional',right=4.0,left=0.0)
+#solver = Newton(equation,line_search,max_iter=40,calc_norm=True,step_method='exact',min_term='functional',
+#				output_file='exact_newton_test',ref_solution=solution_picard_ref)
+solver = Newton(equation,line_search,max_iter=100,calc_norm=True,
+				output_file='2d_high_resolution/Newton_with_exact',ref_solution=solution_picard_ref)
+
+solution_Newton_exact = solver.solve()
+
+
+print('Picard constant')
+# Picard
+equation = FullStokes(Domain,Elements,B,n,delta,mu0,f)
+# We solve the problem with a velocity field that we introduce in nue
+initial_stokes = Stokes(Domain,Elements,B*realistic_factor,f)
+initial_solve = LinearDirect(initial_stokes)
+initial_solve.solve()
+equation.U = initial_stokes.U
+line_search = ConstantStep()
+solver = Picard(equation,line_search,max_iter=100,calc_norm=True,output_file='2d_high_resolution/Picard_constant',ref_solution=solution_picard_ref)
+solution_picard_ref = solver.solve()
+
+print('Picard exact step size')
+# Picard exact step size
+equation = FullStokes(Domain,Elements,B,n,delta,mu0,f)
+# We solve the problem with a velocity field that we introduce in nue
+initial_stokes = Stokes(Domain,Elements,B*realistic_factor,f)
+initial_solve = LinearDirect(initial_stokes)
+initial_solve.solve()
+equation.U = initial_stokes.U
+line_search =Exact(min_term='functional',right=4.0,left=0.0)
+solver = Picard(equation,line_search,max_iter=100,calc_norm=True,
+output_file='2d_high_resolution/Picard_exact',ref_solution=solution_picard_ref)
+solution_picard_ref = solver.solve()
+
+print('Picard armijo step size')
+# Picard exact step size
+equation = FullStokes(Domain,Elements,B,n,delta,mu0,f)
+# We solve the problem with a velocity field that we introduce in nue
+initial_stokes = Stokes(Domain,Elements,B*realistic_factor,f)
+initial_solve = LinearDirect(initial_stokes)
+initial_solve.solve()
+equation.U = initial_stokes.U
+line_search =Armijo(min_term='functional',gamma=10**(-10))
+solver = Picard(equation,line_search,max_iter=100,calc_norm=True,
+output_file='2d_high_resolution/Picard_armijo',ref_solution=solution_picard_ref)
+solution_picard_ref = solver.solve()
+
+end = time.time()
+print('elapsed time',end-start)
+
